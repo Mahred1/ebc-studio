7dab6df0f3a28c367dc7a70eb25bd22cd80a6d23
Mahred1 <mredwan.kemal@gmail.com>
2026-09-05 14:19:32 +0300

    intial home page layout and not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <Link href='/'> Home</Link>
+  )
+}
+
+export default NotFound
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Button>hello</Button>
+      <Link href={'/reservation'}><Button>Reserve a Spot</Button></Link>
+      <Link href={'/check-reservation'}><Button>Check Reservation</Button></Link>
     </div>
   );
 }
