3cd784e49a989655a3ad29e732e6cbf3fce84f65
Mahred1 <mredwan.kemal@gmail.com>
2026-09-05 14:40:19 +0300

    reserve page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Link href={'/reservation'}><Button>Reserve a Spot</Button></Link>
+      <Link href={'/reserve'}><Button>Reserve a Spot</Button></Link>
       <Link href={'/check-reservation'}><Button>Check Reservation</Button></Link>
     </div>
   );
---
+++ app/reserve/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return <main className="h-dvh w-dvw flex items-center justify-center">
+    <div className="flex flex-col justify-center items-center gap-2 tracking-widest">
+    <h1 className="text-primary font-black text-4xl tracking-normal">Welcome to Ethiopian Televison</h1>
+    <h3 className="text-xl font-light ">Reserve Your Spot Now!</h3>
+    </div>
+    
+  </main>;
+};
+
+export default page;
